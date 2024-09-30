b53a233e3d517f4243528fe8ef6efbe1cb8ad190
Niklas <Max_Niklas@outlook.de>
2024-09-30 15:41:33 +0200

    update README

---
+++ README.md
@@ -0,0 +1,74 @@
+# loadpy
+This is a small python packages that provides simple laoding animations for CLI applications:
+
+There are two types of loading animations that are provided by this package:
+1. Throbber
+2. Loading Bar
+
+To use them, import loadpy and either instantiate a Throbber or LoadingBar object.
+
+## Throbber
+<p align="center">
+    <img src="images/throbber.gif" alt="Throbber" height="30">
+</p>
+
+### Parameters
+
+The throbber symbolizes a running process. It constructor has 3 parameters:
+1. `desc` : The description of the process that is being run.
+2. `end`: The end message that is displayed when the process is completed.
+3. `timeout`: The time interval between each throbber symbol. Default is 0.1 seconds.
+
+### Start and stopping a throbber animation
+To start the animation, call the `start` method on the throbber object.  
+To stop the animation, call the `stop` method on the same object.
+
+### Example
+
+```python
+import loadpy
+
+throbber = loadpy.Throbber(desc="Loading", end="Done!", timeout=0.2)
+
+throbber.start()
+time.sleep(4)
+throbber.stop()
+```
+This example generates the animation above. - time.sleep represents a process that takes 4 seconds to complete.
+
+
+## Loading Bar
+<p align="center">
+    <img src="images/loadingBar.gif" alt="Throbber" height="30">
+</p>
+
+### Parameters
+
+The loading bar is used for processes that have repeating steps and thus the percent of currently finished steps can be displayed. It constructor has 2 parameters:
+1. `total` : The total number of steps that are to be completed.
+2. `desc`: The description of the process that is being run.
+
+### Updating the loading bar
+The loading Bar will be initially printed to the console as soon as the object is instantiated. To update the loading bar, call the `load` method on the object and pass the current value ir step of the process.
+
+### Example
+
+```python
+import loadpy
+
+steps = range(100)
+total_steps = len(steps)
+
+loadingBar = loadpy.LoadingBar(total_steps, desc="Loading")
+
+for i in steps:
+    loadingBar.load(i)
+    time.sleep(0.1)
+```
+This example generates the animation above. - time.sleep is used here, so the bar isn't instantly finished.
+
+### Usage & Tips
+The idea is, that if a loop is used that will take a while, the bar can be displayed on how far the process has come. 
+- If the running variable of the loop is a increasing number like above (i), the loading bar can be updated with the current value of i and should be initialized with the total number of i.
+- If the running variable is a list of items, the loading bar can be updated with the index of the item in the list and should be initialized with the length of the list.
+- Generally: As long as we know the number of steps / length of the process, the loading bar can be used.
--- loadpy.py
+++ loadpy.py
@@ -13,41 +13,42 @@ import sys
 
 
 class Throbber:
-    def __init__(self, desc="Loading...", end="Done!", timeout=0.3):
+    def __init__(self, desc="Loading...", end="Done!", timeout=0.1):
         self.desc = desc
         self.end = end
         self.timeout = timeout
-        self._running = False
-        self._thread = None
+        self.__running = False
+        self.__thread = None
 
     def start(self):
-        self._running = True
-        self._thread = threading.Thread(target=self._animate)
-        self._thread.start()
+        self.__running = True
+        self.__thread = threading.Thread(target=self.__animate)
+        self.__thread.start()
 
-    def _animate(self):
+    def __animate(self):
         for c in itertools.cycle(['|', '/', '-', '\\']):
-            if not self._running:
+            if not self.__running:
                 break
             sys.stdout.write(f'\r{self.desc} {c}')
             sys.stdout.flush()
             time.sleep(self.timeout)
+        sys.stdout.write('\r' + ' ' * (len(self.desc) + 2))
         sys.stdout.write(f'\r{self.end}\n')
         sys.stdout.flush()
 
     def stop(self):
-        self._running = False
-        if self._thread is not None:
-            self._thread.join()
+        self.__running = False
+        if self.__thread is not None:
+            self.__thread.join()
 
 
 class LoadingBar:
-    _bar_end = "] % finished"
+    __bar_end = "] % finished"
 
     def __init__(self, total, desc="Loading"):
         self.total = total - 1
-        self.finished = False
-        self.bar_start = desc + ": ["
+        self.__finished = False
+        self.__bar_start = desc + ": ["
         self.__percent = -1
         self.load(0)
         self.__percent = 0
@@ -57,24 +58,24 @@ class LoadingBar:
 
         if self.__percent < current_percent < 100:
             self.__percent = current_percent
-            self._print_bar(current_percent)
+            self.__print_bar(current_percent)
 
-        elif not self.finished and current_percent == 100:
-            self._print_bar(100, final=True)
+        elif not self.__finished and current_percent == 100:
+            self.__print_bar(100, final=True)
 
         else:
             return
 
-    def _print_bar(self, current_percent, final=False):
+    def __print_bar(self, current_percent, final=False):
         current_percent_string = f" {current_percent:02.0f}"
         repetitions = int(math.floor(math.floor(current_percent) / 10.0))
 
-        bar = self.bar_start
+        bar = self.__bar_start
         bar = bar + " ##" * (repetitions if repetitions < 9 else 9) + current_percent_string
-        bar += " --" * (10 - repetitions - 1) + ("" if current_percent >= 100 else " ") + self._bar_end
+        bar += " --" * (10 - repetitions - 1) + ("" if current_percent >= 100 else " ") + self.__bar_end
 
         if final:
-            self.finished = True
+            self.__finished = True
             sys.stdout.write(f'\r{bar}\n\n')
         else:
             sys.stdout.write(f'\r{bar}')
